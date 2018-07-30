58642e4f240ea21348fa0696a01e9ddd2cc72da2
chintha123 <41770310+chintha123@users.noreply.github.com>
2018-07-30 15:13:48 +0530

    Update Even.java

--- Even.java
+++ Even.java
@@ -5,16 +5,17 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Even
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	    Scanner sc=new Scanner(System.in);
 	    int n=sc.nextInt();
-	    if(n%2==0)
+	    if(n<0)
+	          System.out.println("invalid");
+	     else if(n%2==0)
 	          System.out.println("Even");
-	     else
-	          System.out.println("Odd");
-	          
+	        else
+	            System.out.println("Odd");
 	}
 }
