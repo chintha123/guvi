11bf08ac549c479f1c781ef6d248683be874a3d3
chintha123 <41770310+chintha123@users.noreply.github.com>
2018-07-30 16:14:06 +0530

    Update Alpha.java

--- Alpha.java
+++ Alpha.java
@@ -13,7 +13,7 @@ class Alpha
 		char c=sc.next().charAt(0);
       	if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'||c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
         	System.out.println("Vowel");
-        else if((c>'a'&&c<'z')||(c>'A'&&c<'Z'))
+        else if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
         	     System.out.println("Consonant");
         	 else
         	       System.out.println("invalid");
