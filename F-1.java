a156317f6bcad2eb5a6e789bac773754dc430825
chintha123 <41770310+chintha123@users.noreply.github.com>
2018-08-16 12:50:34 +0530

    Create F-1.java

---
+++ F-1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        int a[]=new int[20];
+        int temp=n;
+        while(n>0)
+        {
+        	n=n/10;
+        	count++;
+        }
+        for(int i=0;i<count;i++)
+        {
+        a[i]=temp%10;
+        temp=temp/10;
+	}
+	for(int i=count-1;i>=0;i--)
+	System.out.print(a[i]+" ");
+	}
+}
