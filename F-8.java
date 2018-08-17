b10a28edfce55a362c96d098fea24971125a8865
chintha123 <41770310+chintha123@users.noreply.github.com>
2018-08-17 11:49:21 +0530

    Create F-8.java

---
+++ F-8.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner sc=new Scanner(System.in);
+	   int n=sc.nextInt();
+	   int k=sc.nextInt();
+	   int a[]=new int[n];
+	   int count=0;
+	   for(int i=0;i<n;i++)
+	   a[i]=sc.nextInt();
+	   for(int i=0;i<n;i++)
+	   {
+	   	if(k==a[i])
+	   	count++;
+	   }
+	   if(count==0)
+	   System.out.println("no");
+	   else
+	     System.out.println("yes");
+	}
+}
