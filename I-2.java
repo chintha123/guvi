2d461882d051b535b08a566871e59a8af58c1d48
chintha123 <41770310+chintha123@users.noreply.github.com>
2018-08-21 13:29:33 +0530

    Update I-2.java

--- I-2.java
+++ I-2.java
@@ -10,9 +10,9 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		float n1=sc.nextFloat();
-            	float n2=sc.nextFloat();
-	            float res=n1*n2;
+		double n1=sc.nextDouble();
+            	double n2=sc.nextDouble();
+	            double res=n1*n2;
                         	System.out.format("%.5f",res);
 	}
 }
