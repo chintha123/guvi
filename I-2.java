d866ffa20ae451d83d108224ae3f0ad354df91c4
chintha123 <41770310+chintha123@users.noreply.github.com>
2018-08-21 13:00:48 +0530

    Create I-2.java

---
+++ I-2.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		float n1=sc.nextFloat();
+            	float n2=sc.nextFloat();
+	            float res=n1*n2;
+                        	System.out.format("%.5f",res);
+	}
+}
