0983e03b459bda1c467799862aa8460e5c7eb740
chintha123 <41770310+chintha123@users.noreply.github.com>
2018-08-22 11:06:24 +0530

    Create J-7.java

---
+++ J-7.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int d;
+		while(n>0)
+		{
+		 d=n%10;
+		System.out.print(d);
+		n=n/10;
+		}
+	}
+}
