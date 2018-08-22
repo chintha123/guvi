ddc225dffb3d79721c011f3e2d5959f07bafb948
chintha123 <41770310+chintha123@users.noreply.github.com>
2018-08-22 10:04:25 +0530

    Create J-1.java

---
+++ J-1.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int l=sc.nextInt();
+		int w=sc.nextInt();
+	           int h=sc.nextInt();
+	           int area=2*(l*w+w*h+h*l);
+	           int volume=l*w*h;
+	           System.out.println(area+" "+volume);
+	}
+}
