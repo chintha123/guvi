7d12e3b10d10d246274481df87c0187a6de7e711
chintha123 <41770310+chintha123@users.noreply.github.com>
2018-08-22 10:09:13 +0530

    Update J-1.java

--- J-1.java
+++ J-1.java
@@ -13,7 +13,7 @@ class Ideone
 		int l=sc.nextInt();
 		int w=sc.nextInt();
 	           int h=sc.nextInt();
-	           int area=2*(l*w+w*h+h*l);
+	           int area=2*(l*h+w*h+w*l);
 	           int volume=l*w*h;
 	           System.out.println(area+" "+volume);
 	}
