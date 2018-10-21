2aba0d1b2976dd3ec30bb176f477d7d45feb4fc7
prasad <41770310+chintha123@users.noreply.github.com>
2018-10-21 16:23:31 +0530

    Create Hun1.java

---
+++ Hun1.java
@@ -0,0 +1,46 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int a[]=new int[n];
+    for(int i=0;i<n;i++)
+     a[i]=sc.nextInt();
+      Arrays.sort(a);
+     int p=-1;
+     int uni=0;
+      for(int i=0;i<n;i++)
+      {
+         int count=1;
+         for(int j=i+1;j<n;j++)
+         {
+              if(a[i]==a[j]){
+               count++;
+                uni++;
+               }
+               else
+                 break;
+           }
+       if(count>1)
+        {
+            if(i==n-1)
+             System.out.print(a[i]);
+             else
+               System.out.print(a[i]+" ");
+          }
+    p=p+count;
+     i=p;
+   }
+if(uni==0)
+System.out.print("unique");
+	}
+}
