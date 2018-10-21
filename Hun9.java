c8433c7784b5af3816dca47b09ea90879b898093
prasad <41770310+chintha123@users.noreply.github.com>
2018-10-21 20:17:59 +0530

    Create Hun9.java

---
+++ Hun9.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here	Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int a[]=new int[n];
+	    for(int i=0;i<n;i++)
+	    a[i]=sc.nextInt();
+	    for(int i=0;i<n;i++)
+	    {
+	    	for(int j=i+1;j<n;j++)
+	    	{
+	    		int res=a[i]+a[j];
+	    			 if(res==0)
+	    			System.out.println(a[i]+" "+a[j]);
+
+	    	}
+	    }
+	}
+}
