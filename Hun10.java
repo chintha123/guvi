111cfcb77063cce098244a4f0ba352820e95b310
prasad <41770310+chintha123@users.noreply.github.com>
2018-10-21 21:47:32 +0530

    Create Hun10.java

---
+++ Hun10.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class ArraySubset
+{
+   public static void main(String[] args)
+   {
+     Scanner sc=new Scanner(System.in);
+		
+     int m=sc.nextInt();
+	
+     int n=sc.nextInt();
+	
+     int a[]=new int[m];
+	
+	int b[]=new int[n];
+	
+	for(int i=0;i<m;i++)
+	
+	a[i]=sc.nextInt();
+	
+	for(int j=0;j<n;j++)
+	
+	b[j]=sc.nextInt();
+    int count=0;
+    for(int i=0;i<m;i++)
+    {
+      for(int j=0;j<n;j++)
+       {
+          if(a[i]==b[j]){
+           count++;
+           break;
+          }
+       }
+    }
+if(count==n)
+System.out.print("YES");
+else
+System.out.print("NO");
+}
+}
