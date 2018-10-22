1dc3f0c92db6af5ff7922d1688be94dd1a5c76cc
prasad <41770310+chintha123@users.noreply.github.com>
2018-10-22 15:37:56 +0530

    Create Hun12.java

---
+++ Hun12.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		int num=sc.nextInt();
+		for(int i=0;i<n;i++)
+		a[i]=sc.nextInt();
+		Arrays.sort(a);
+		System.out.print(a[n-num]);
+	}
+}
